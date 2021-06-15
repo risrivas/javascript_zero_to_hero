4d883f493a2b8c6017542111957e44f731003279
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-16 07:19:49 +0800

    correct usage of variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,13 @@
 let js = 'amazing';
-if (js === 'amazing') {
+/* if (js === 'amazing') {
     alert('JavaScript is FUN!');
-}
+} */
+
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Rishi";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
