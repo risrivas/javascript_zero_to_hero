cef8d610c312122eee9c9183f848b1211bc8e445
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-17 07:38:35 +0800

    assignment done and started data types

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+/* let country = "India";
+let continent = "Asia";
+let population = 200_000_000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+ */
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,35 @@
-let js = 'amazing';
+// let js = 'amazing';
 /* if (js === 'amazing') {
     alert('JavaScript is FUN!');
 } */
 
-console.log(40 + 8 + 23 - 10);
+/* console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
 let firstName = "Rishi";
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+console.log(firstName); */
+
+let javaScriptIsFun = true;
+
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof false);
+
+javaScriptIsFun = 'YES!';
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
