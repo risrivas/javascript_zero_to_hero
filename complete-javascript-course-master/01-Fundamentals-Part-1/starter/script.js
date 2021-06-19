24b506b2c5f18deef4b2153b8a7f2d5fa5be5d2e
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-20 07:55:01 +0800

    basic operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -33,7 +33,6 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -47,3 +46,28 @@ job = 'teacher';
 
 lastName = 'Srivastava';
 console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2019;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+
+const firstName = 'Rishi';
+const lastName = 'Srivastava';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
