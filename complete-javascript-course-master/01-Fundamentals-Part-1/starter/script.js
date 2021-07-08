da20f246c60ecdb4109cd14d2fd15a6a0ddffb84
Rishi Srivastava <rishisrivasta@gmail.com>
2021-07-09 07:15:54 +0800

    if else condition

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -86,7 +86,7 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
-*/
+
 
 const firstName = 'Jonas';
 const job = 'teacher';
@@ -106,6 +106,25 @@ multiple \n\
 lines');
 
 console.log(`String with
-multiple 
+multiple
 lines`);
+*/
 
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 😱');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years.`)
+}
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
