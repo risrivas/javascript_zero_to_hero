4f07e2c36573fd9710d54d6b8a87bf1b24926ed6
Rishi Srivastava <rishisrivasta@gmail.com>
2021-07-19 07:21:44 +0800

    truthy and falsy values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -129,7 +129,6 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 
-*/
 
 // type conversion
 const inputYear = '1991';
@@ -158,4 +157,23 @@ let n = '1' + 1;
 n = n - 1;
 console.log(n); // 10
 
+*/
 
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log(`Don't spend it all ;)`);
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log('Height is defined!');
+} else {
+    console.log('Height is UNDEFINED!');
+}
